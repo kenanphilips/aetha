671244b71680733bb46cec4333c007eb2a3e5318
Kenan Philips <philipskenan@gmail.com>
2018-03-02 15:24:44 -0800

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-$document.ready(function(){
-  $('[data-spy="scroll"]').each(function () {
-    var $spy = $(this).scrollspy('refresh')
-  }); 
+$(document).ready(function () {
+  $('.nav-link').on('click', function(e) {
+    e.preventDefault();
+    var section = $(this).attr('href').substr($(this).attr('href').indexOf('#'));
+    var $section = $(section);
+
+    $('html, body').animate({
+      scrollTop: $section.offset().top + -100
+    }, 1000);
+  });
+
+  $('.navbar-brand').click(function() {
+    $('html, body').animate({scrollTop: 0}, 1000);
+    return false;
+  });
 });
